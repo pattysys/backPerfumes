339c0d1fb50e2244600cf968f88895d31bd0ef3e
pattysys <sandra.munevar.mt@correo.usa.edu.co>
2021-11-28 07:57:46 -0500

    se actualiza aplication propertties

--- src/main/java/com/Zorrilo/Zorrillo/ZorrilloApplication.java
+++ src/main/java/com/Zorrilo/Zorrillo/ZorrilloApplication.java
@@ -1,7 +1,9 @@
 package com.Zorrilo.Zorrillo;
 
+
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+
 
 @SpringBootApplication
 public class ZorrilloApplication {
